63befc5cfcfeebd5fc51e7c5c05b4b61e7165c14
ziziw <ziwangchen@hotmail.com>
2017-08-08 20:15:52 -0400

    first commit

---
+++ age calculator.js
@@ -0,0 +1,5 @@
+var age = prompt("how old are you?");
+
+var days = age * 365.25;
+
+alert(age + " years is roughly " + days + " days");
---
+++ script.js
@@ -0,0 +1,8 @@
+var firstName = prompt("what is your first name?");
+
+var lastName = prompt("what is your last name?");
+
+var age = prompt("what is your age?");
+
+console.log("your full name is " + firstName + " " + lastName);
+console.log("your age is " + age);
