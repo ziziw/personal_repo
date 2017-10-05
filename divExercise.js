f93e05ed5ab89577812096427d7c26a19a3481bb
ziziw <ziwangchen@hotmail.com>
2017-10-05 19:03:46 -0400

    tried smth

--- divExercise.js
+++ divExercise.js
@@ -1,4 +1,5 @@
 $("div").css("background-color", "purple");
 $(".highlight").css("width", "200px");
 $("#third").css("border", "solid orange");
-$("div:first").css("color", "pink");
+//$("div:first").css("color", "pink");
+$("div").first().css("color", "pink");
