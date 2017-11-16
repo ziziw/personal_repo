3c1aa27ae0f78e33d13dc00e979cfb9675c3a2ae
ziziw <ziwangchen@hotmail.com>
2017-11-15 23:05:49 -0500

    failed today

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -2,8 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res){
-	res.render("home.html");
-	res.send("<h1>Welcome to the home page!</h1><h2>blah blah blah</h2>")
+	res.render("home.ejs");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
